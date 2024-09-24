2f99fc6e12c5f25422faabc059c9eb293ca0567e
Fathi XII B <shiroyukiaether@gnmail.com>
2024-09-24 15:15:47 +0800

    Selasa, 240924

--- src/app.js
+++ src/app.js
@@ -15,10 +15,58 @@ document.addEventListener('alpine:init', () => {
         total: 0,
         quantity: 0,
         add(newItem)  {
-            this.items.push(newItem);
+            // cek apakah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            //jika belum ada / cart masih kosong
+            if(!cartItem) {
+            this.items.push({...newItem, quantity: 1, total: newItem.price});
             this.quantity++;
-            this.total += newItem.price;
-            console.log(this.items);
+            this.total += newItem.price;   
+            } else {
+                // jika barang sudah ada, cek apakah barang beda atau sama dengan yang ada di cart
+                this.items = this.items.map((item) => {
+                    //jika barang berbeda
+                    if(item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        // jika barang sudah ada, tambah quantity dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += Item.price;
+                        return item;  
+                    }
+                });
+            }
+
+            console.log(this.total);
+        },
+        remove(id) {
+            //ambil item yang mau diremove berdasarkan id nya
+            const cartItem = this.items.find((item) => item.id === id);
+
+            //jika item lebih dari 1
+            if(cartItem.quantity > 1) {
+                //telusuri 1 1
+                this.items = this.items.map((item) => {
+                    // jika bukan barang yang diklik
+                    if(item.id !== id) {
+                        return item;
+                    }   else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            } else if (cartItem.quantity === 1) {
+                //jika barangnya sisa 1
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
         },
     });
 });
