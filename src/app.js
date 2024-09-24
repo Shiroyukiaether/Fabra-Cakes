bac12e0df1401bda77e0b8b6b30fcd2310d4a79a
Fathi XII B <shiroyukiaether@gnmail.com>
2024-09-24 11:50:59 +0800

    yeayy

--- src/app.js
+++ src/app.js
@@ -9,4 +9,25 @@ document.addEventListener('alpine:init', () => {
             { id: 6, name: 'Cheese Cake', img: '6.jpeg', price: 280000 },
         ],
     }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem)  {
+            this.items.push(newItem);
+            this.quantity++;
+            this.total += newItem.price;
+            console.log(this.items);
+        },
+    });
 });
+
+// konversi ke Rupiah
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0, 
+    }).format(number);
+};
