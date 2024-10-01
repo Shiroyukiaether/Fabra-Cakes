121b8068f23bd47dd51165cdd894e3b1b5262a43
Fth <134037373+Shiroyukiaether@users.noreply.github.com>
2024-10-02 06:13:23 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -104,7 +104,7 @@ checkoutButton.addEventListener('click', function (e) {
 const formatMessage = (obj) => {
     return `Data Customer
 Nama: ${obj.name}
-Alamat: ${obj.adress}
+Alamat: ${obj.address}
 No. HP: ${obj.phone}
 Data Pesanan
 ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`)}
